8b727c1a9d27888a930b5ec4996d87917a101aed
Rob Rose <bertra97@gmail.com>
2017-04-11 03:56:11 -0400

    Worked on CS 341 Project 3
    - Added stuff to rebalance in LazyBST.cpp
    - Removed the child disconnect from flattenNodes and added another
      method to do that for the rebalancing.
    - Worked on the insert function with a kind of Binary Search vibe. Still
      quite far to go on it.
    - Still have a bunch of work to do but I need to go to sleep.

--- umbc-cs341/Projects II/Project 3/LazyBST.cpp
+++ umbc-cs341/Projects II/Project 3/LazyBST.cpp
@@ -72,20 +72,20 @@ bool LazyBST::locate(const char *position, int& key) {
 }
 
 
-void LazyBST::flattenAndDisconnectNodes(
+void LazyBST::flattenNodes(
         Node* on, int& index, Node** arr, int size ) {
 
     if(on->m_left != NULL) {
         flattenNodes(on->m_left, index, arr, size);
-        on->m_left = NULL;
     }
 
     arr[index] = on;
-    on++;
+    index++;
 
     if(on->m_right != NULL) {
-
+        flattenNodes(on->m_right, index, arr, size);
     }
+
 }
 
 
@@ -114,7 +114,8 @@ Node* LazyBST::insertAndRecurr(Node* on, int key) {
             
             if(result == NULL) {
 
-                // No node was inserted, just recurr up the tree doing nothing
+                // No node was inserted, just recurr up the tree doing 
+                // nothing
                 return NULL;
 
             } else {
@@ -142,7 +143,8 @@ Node* LazyBST::insertAndRecurr(Node* on, int key) {
             
             if(result == NULL) {
 
-                // No node was inserted, just recurr up the tree doing nothing
+                // No node was inserted, just recurr up the tree doing 
+                // nothing
                 return NULL;
 
             } else {
@@ -186,28 +188,75 @@ Node* LazyBST::rebalanceAndRecurr(Node* on) {
 
     int hLeft; 
     if(on->m_left != NULL) {
+        // Recurr on the left size first.
         on->m_left = rebalanceAndRecurr(on->m_left);
         hLeft = on->m_left->m_height;
     } else {
+        // Left side is empty.
         hLeft = 0;
     }
 
 
     int hRight;
     if(on->m_right != NULL) {
+        // Then recurr on the right side if there's stuff there. 
         on->m_right = rebalanceAndRecurr(on->m_right);
         hRight = on->m_right->m_height;
-    } else {
+    } else { 
+        // Right side is empty.
         hRight = 0;
     }
      
 
     if(abs(hLeft - hRight) >= min(hLeft, hRight) ) {
 
-               
+        int subTreeSize = on->m_size;
+
+        int flattenIndex = 0;
+        Node* arr[subTreeSize];
+
+        flattenNodes(on, *flattenIndex, arr, subTreeSize);
+        unlinkAllFromChildren(arr, subTreeSize);
+
+        Node* newRoot = insertWithOrder(0, subTreeSize - 1, arr);
 
     }
 
 }
 
 
+Node* LazyBST::insertWithOrder(int lower, int upper, Node** arr) {
+    
+    int middle = lower + (upper - lower) / 2;
+    
+    Node* toReturn = arr[middle];
+
+    // Catch to make sure we haven't reached the end.
+    if(upper != lower) {
+
+        toReturn->m_left = insertWithOrder(lower, middle, arr);
+        toReturn->m_right = insertWithOrder(middle, upper, arr);
+
+    }
+
+    return toReturn;
+
+}
+
+
+// This separates all the nodes in a flattened BST from their children. This
+// allows the rebalancing to take place more easily. It also sets all their
+// heights to 0 and sizes to 1.
+
+void LazyBST::unlinkAllFromChildren(Node** arr, int size) {
+    for(int i = 0; i < size; i++) {
+
+        arr[i]->m_height = 0;
+        arr[i]->m_sizes = 1;
+        arr[i]->m_left = NULL;
+        arr[i]->m_right = NULL;
+
+    }
+    
+
+}
