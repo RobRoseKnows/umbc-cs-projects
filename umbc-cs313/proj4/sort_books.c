dd3b6c2cd6d3e2495f32f807c8b08818db934c92
Rob Rose <bertra97@gmail.com>
2017-04-11 23:51:56 -0400

    Revert "Finished Project 4"
    This reverts commit 33bcd37629dc0d328d877c73c31358efbb1a0ba6.

--- umbc-cs313/proj4/sort_books.c
+++ umbc-cs313/proj4/sort_books.c
@@ -19,15 +19,11 @@ struct book {
 };
 
 
-void sort_books(struct book books[], int numBooks);
-
-void print_books(struct book books[], int numBooks);
-
 /*
  * Declarations for functions that are defined in other files
  */
 
-/* Tell the compiler that bookcmp is external (our assembly subroutine) */
+// Tell the compiler that bookcmp is external (our assembly subroutine)
 extern int bookcmp(void);
 
 /*
@@ -43,7 +39,6 @@ struct book* book2 = NULL;
 int main(int argc, char **argv) {
     struct book books[MAX_BOOKS];
     int numBooks;
-    int i;
 
     for (i = 0; i < MAX_BOOKS; i++) {
         /* Sample line: "Breaking Point, Pamela Clare, Romance, 2011" */
@@ -63,16 +58,16 @@ int main(int argc, char **argv) {
          * be read directly into the struct book's members.
          */
 
-        struct book* currBook = &books[i];
+        struct book* currBook = books[i];
 
         char titleBuffer [TITLE_BUF_LEN];
-        char* titlePtr = currBook->title;
-        char* authorPtr = currBook->author;
-        char* subjectPtr = currBook->subject;
-        unsigned int* yearPtr = &(currBook->year);
+        char* titlePtr = &(currBook->title);
+        char* authorPtr = &(currBook->author);
+        char* genrePtr = &(currBook->genre);
+        int* yearPtr = &(currBook->year);
 
-        int numFields = scanf("%80[^,], %20[^,], %10[^,], %u \n",
-                titleBuffer, authorPtr, subjectPtr, yearPtr);
+        numFields = scanf("%80[^,], %20[^,], %10[^,], %u \n",
+                titleBuffer, authorPtr, genrePtr, yearPtr);
 
         if (numFields == EOF) {
             numBooks = i;
@@ -85,7 +80,7 @@ int main(int argc, char **argv) {
          * Then, post-process title (see project spec for reason)
          */
 
-        /* Check to make sure we got the right number of fields. */
+        // Check to make sure we got the right number of fields.
         if(numFields != 4) {
             printf("Invalid number of fields in book on line %d", i);
             exit(1);
@@ -103,7 +98,7 @@ int main(int argc, char **argv) {
 
     print_books(books, numBooks);
 
-    exit(1);
+    exit(0);
 }
 
 /*
@@ -126,17 +121,16 @@ void sort_books(struct book books[], int numBooks) {
             /* Copy pointers to the two books to be compared into the
              * global variables book1 and book2 for bookcmp() to see
              */
-            book1 = &books[min];
-            book2 = &books[j];
+            book1 = books[min];
+            book2 = books[j];
 
             cmpResult = bookcmp();
             /* bookcmp returns result in register EAX--above saves
             * it into cmpResult */
     
-            /* We want to check if book2 is less than book1, since bookcmp()
-             * compares book1 ??? book2, if the number returned by bookcmp()
-             * (cmpResult) is one, it means it's greater than.
-             */
+            // We want to check if book2 is less than book1, since bookcmp()
+            // compares book1 ??? book2, if the number returned by bookcmp()
+            // (cmpResult) is one, it means 
             if (cmpResult == 1) {
                 min = j;
             }
@@ -157,7 +151,7 @@ void print_books(struct book books[], int numBooks) {
 
     for(i = 0; i < numBooks; i++) {
 
-        printf("%s, %s, %s, %d", books[i].title, books[i].author, books[i].subject, books[i].year);
+        printf("
 
     }
 
