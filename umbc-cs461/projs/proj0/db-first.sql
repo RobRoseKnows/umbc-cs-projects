930ef0c9b82a3020a0cf156725a5bb86a44e3bed
Rob Rose <RobRoseKnows+Git@gmail.com>
2019-02-12 22:19:11 -0500

    Did Databases project 0

---
+++ umbc-cs461/projs/proj0/db-first.sql
@@ -0,0 +1,13 @@
+-- List existing available database
+SHOW DATABASES;
+--
+-- create a database if you didn't do so already, say with name 'university'
+CREATE DATABASE university;
+--
+-- Choose a database to work with, say 'university'
+USE university;
+--
+SET SQL_SAFE_UPDATES = 0;
+--
+-- continue with the rest of your SQL statements (create tables, queries, etc)
+--
