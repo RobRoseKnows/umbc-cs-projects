76349130dca65acf1d7e6c8b20c249882be55dca
Rob Rose <RobRoseKnows+Git@gmail.com>
2019-04-08 21:06:25 -0400

    Finish problem 7

--- umbc-cs461/hws/hw3/hw3p7.sql
+++ umbc-cs461/hws/hw3/hw3p7.sql
@@ -1,1 +1,62 @@
+create table customer
+    (
+        `email`     varchar(255) not null,
+        `name`      varchar(32) not null,
+        `gender`    varchar(1) not null,
+        `city`      varchar(16),
+        `state`     varchar(2),
+        primary key(`email`)
+    )
 
+create table dealer
+    (
+        `website`   varchar(255) not null,
+        `name`      varchar(32) not null,
+        `city`      varchar(32),
+        `state`     varchar(2),
+        primary key(`website`)
+    )
+
+create table vehicle
+    (
+        `vin`           varchar(17) not null,
+        `make`          varchar(16) not null,
+        `model`         varchar(16) not null,
+        `year`          numeric(4,0) not null, check (`year` > 1900),
+        `bought_from`   varchar(255) not null,
+        `sold_to`       varchar(255),
+        primary key(`vin`),
+        foreign key(`bought_from`) references dealer(`website`),
+        foreign key(`sold_to`) references customer(`email`)
+        foreign key(`make`, `model`, `year`) references car(`make`, `model`, `year`)
+    )
+
+create table car
+    (
+        `make`      varchar(16) not null,
+        `model`     varchar(16) not null,
+        `year`      numeric(4,0) not null, check (`year` > 1900),
+        primary key(`make`, `model`, `year`)
+    )
+
+create table car_option
+    (
+        `make`      varchar(16) not null,
+        `model`     varchar(16) not null,
+        `year`      numeric(4,0) not null, check (`year` > 1900),
+        `option`    varchar(16) not null,
+        primary key(`make`, `model`, `year`, `option`),
+        foreign key(`make`, `model`, `year`) references car(`make`, `model`, `year`)
+    )
+
+create table has_options
+    (
+        `make`      varchar(16) not null,
+        `model`     varchar(16) not null,
+        `year`      numeric(4,0) not null, check (`year` > 1900),
+        `option`    varchar(16) not null,
+        `vin`       varchar(17) not null,
+        primary key(`make`, `model`, `year`, `option`, `vin`),
+        foreign key(`make`, `model`, `year`) references car(`make`, `model`, `year`),
+        foreign key(`vin`) references vehicle(`vin`)
+    )
